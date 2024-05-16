49cbb6bbd1b6c655c9bdd36ead79680e1352cc25
santnamsingh <satnamsingh@vancedsolutions.com>
2024-05-16 17:53:57 +0530

    Add controller

---
+++ API/Controllers/ImportantNumberController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Common.Dtos;
+using Application.Handler.ImportantNumber.Queries.GetAllImportantNumbers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ImportantNumberController : BaseController
+    {
+        #region Commands
+
+        #endregion
+
+        #region Queries
+        [HttpPost]
+        [Route("GetAllImportantNumbers")]
+        public async Task<IActionResult> GetAllImportantNumbers([FromBody] ServerRowsRequest commonRequest)
+        {
+            var result = await Mediator.Send(new GetAllImportantNumbersQuery { CommonRequest = commonRequest });
+            return Ok(result);
+        }
+        #endregion
+    }
+}
